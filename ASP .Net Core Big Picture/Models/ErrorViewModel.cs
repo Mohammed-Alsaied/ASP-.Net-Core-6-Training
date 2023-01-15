29ae0ceda29931794eee2445a115157f84e31113
Mohamed Alsaied <mohamed.saied.civil@gmail.com>
2023-01-15 22:18:53 +0200

    Add project files.

---
+++ ASP .Net Core Big Picture/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_.Net_Core_Big_Picture.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
